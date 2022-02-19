352da43ec0256be943cc3b634b30b1db0e33f39b
YoungKyonYou <man.spride1324@gmail.com>
2022-02-19 16:02:34 +0900

    initial repository

---
+++ src/main/java/com/asiana/lawgic/lawgic/repository/ChatRepository.java
@@ -0,0 +1,6 @@
+package com.asiana.lawgic.lawgic.repository;
+
+import com.asiana.lawgic.lawgic.entity.Chat;
+
+public interface ChatRepository extends JpaRepository<Chat,String> {
+}
---
+++ src/main/java/com/asiana/lawgic/lawgic/repository/ClientRepository.java
@@ -0,0 +1,6 @@
+package com.asiana.lawgic.lawgic.repository;
+
+import com.asiana.lawgic.lawgic.entity.Client;
+
+public interface ClientRepository extends JpaRepository<Client,String> {
+}
