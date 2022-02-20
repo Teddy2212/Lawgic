1861b4a4a22453b2bfc1e2bea304421bed401770
dongooree <pde0406@gmail.com>
2022-02-20 19:32:03 +0900

    add clientVO

---
+++ src/main/java/com/asiana/lawgic/lawgic/vo/ClientVO.java
@@ -0,0 +1,26 @@
+package com.asiana.lawgic.lawgic.vo;
+
+import com.asiana.lawgic.lawgic.entity.CarType;
+
+import java.sql.Date;
+
+public class ClientVO
+{
+    private Long clientId;
+
+    private String password;
+
+    private String name;
+
+    private Date birthday;
+
+    private String email;
+
+    private String address;
+
+    private String phone;
+
+    private CarType carType;
+
+    private int gender;
+}
