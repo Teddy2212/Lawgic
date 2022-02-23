695e87336cc9dd6b80dacc9dda1a82de99105ffc
Jaeuk Oh <58154633+wooko5@users.noreply.github.com>
2022-02-23 09:12:20 +0900

    Revert "0222 내역 push (#13)" (#14)

--- src/main/java/com/asiana/lawgic/lawgic/dao/CategoryDAOMemory.java
+++ src/main/java/com/asiana/lawgic/lawgic/dao/CategoryDAOMemory.java
@@ -39,15 +39,8 @@ public class CategoryDAOMemory implements CategoryDAO{
             String name=rs.getString("name");
             String phone=rs.getString("phone");
             boolean valid=rs.getBoolean("valid");
+            category=Category.getCategory(categoryId);
 
-            if(categoryId==1)
-                category=Category.CAR_TO_CAR;
-            else if(categoryId==2)
-                category=Category.CAT_TO_PERSON;
-            else if(categoryId==3)
-                category=Category.CAR_ONLY;
-            else
-                category=Category.RAILROAD_CROSSING;
 
             Lawyer lawyer=Lawyer.builder().
                     address(address).
@@ -64,4 +57,42 @@ public class CategoryDAOMemory implements CategoryDAO{
         }
         return lawyers;
     }
+
+    @Override
+    public Collection<Lawyer> getLawyersByCategoryId(int category_id) throws SQLException {
+        PreparedStatement pstmt=null;
+        ResultSet rs=null;
+        List<Lawyer> lawyers=new ArrayList<>();
+        Category category=null;
+        sql="SELECT address, category_id, email, gender, lawyer_id, name, password, phone, valid from lawyer where category_id="+category_id;
+        pstmt = conn.prepareStatement(sql);
+        rs = pstmt.executeQuery();
+
+        while(rs.next()){
+            Long lawyerId=rs.getLong("lawyer_id");
+            String password=rs.getString("password");
+            String address=rs.getString("address");
+            int categoryId=rs.getInt("category_id");
+            String email=rs.getString("email");
+            boolean gender=rs.getBoolean("gender");
+            String name=rs.getString("name");
+            String phone=rs.getString("phone");
+            boolean valid=rs.getBoolean("valid");
+
+            Lawyer lawyer=Lawyer.builder().
+                    address(address).
+                    categoryId(category).
+                    email(email).
+                    gender(gender).
+                    lawyerId(lawyerId).
+                    name(name).
+                    password(password).
+                    phone(phone).
+                    valid(valid).
+                    build();
+            lawyers.add(lawyer);
+        }
+        return lawyers;
+    }
+
 }
